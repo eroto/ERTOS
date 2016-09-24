0ac0bba5e235b60754faa5d32853d6bd82d061e4
eroto <eroto1981@gmail.com>
2016-09-23 22:44:02 -0500

    Add interface for IO
    Add interface for IO to make IO config easier

--- IO/io.c
+++ IO/io.c
@@ -6,37 +6,86 @@
  */
 #include "io.h"
 
+void io_init(void)
+{
+    io_Pin_Cfg(PORT_C, 7, kGPIO_DigitalOutput);
+    io_Pin_Cfg(PORT_C, 6, kGPIO_DigitalOutput);
+    io_Pin_Cfg(PORT_C, 5, kGPIO_DigitalOutput);
+}
 
-void io_Pin_Cfg(GPIO_Type GPIO, PORT_Type PORT, uint8_t PIN, gpio_pin_direction_t DIR)
+
+int8_t io_Pin_Cfg(IO_PORT IO_PORT, uint8_t PIN, gpio_pin_direction_t DIR)
 {
 
+    int8_t result = 0;
 	gpio_pin_config_t GPIO_Struct;
 	port_pin_config_t PORT_Struct;
+	clock_ip_name_t port_clk;
+	GPIO_Type* GPIO;
+	PORT_Type* PORT;
+
+	switch(IO_PORT)
+	{
+	    case PORT_A:
+	        port_clk = kCLOCK_PortA;
+	        GPIO = GPIOA;
+	        PORT = PORTA;
+	        break;
+
+	    case PORT_B:
+	        port_clk = kCLOCK_PortB;
+	        GPIO= GPIOB;
+	        PORT= PORTB;
+	        break;
+
+	    case PORT_C:
+	        port_clk = kCLOCK_PortC;
+	        GPIO = GPIOC;
+	        PORT = PORTC;
+	        break;
+
+	    case PORT_D:
+	        port_clk = kCLOCK_PortD;
+	        GPIO = GPIOC;
+	        PORT= PORTC;
+	        break;
+
+	    case PORT_E:
+	        port_clk = kCLOCK_PortE;
+	        GPIO = GPIOE;
+	        PORT = PORTE;
+	        break;
+
+	    default:
+	        result = -1;
+	}
+
+	if(result == 0)
+	{
+	    /* Unlock the port clock */
+        CLOCK_EnableClock(port_clk);
 
 
-	GPIO_Struct.pinDirection = DIR;
-	GPIO_Struct.outputLogic = DIR;
+        // Define a digital input pin PCR configuration
+        PORT_Struct.pullSelect = kPORT_PullUp;
+        PORT_Struct.mux = kPORT_MuxAsGpio;
+        PORT_Struct.passiveFilterEnable = kPORT_PassiveFilterDisable;
+        PORT_Struct.driveStrength = kPORT_LowDriveStrength;
+        PORT_Struct.slewRate = kPORT_SlowSlewRate;
 
+        PORT_SetPinConfig(PORT, 7U, &PORT_Struct);
 
+        GPIO_Struct.pinDirection = DIR;
+        GPIO_Struct.outputLogic = DIR;
+        GPIO_PinInit(GPIO,PIN,&GPIO_Struct);
+	}
+	else
+	{/*Do nothing*/}
 
-	GPIO_PinInit(GPIO,PIN,&PinStruct);
+	return result;
+
 }
 
 
-void TestPin_Setup(void)
-{
-	/* Ungate the port clock */
-	CLOCK_EnableClock(kCLOCK_PortC);
-
-	// Define a digital input pin PCR configuration
-	Test_PIN_Setup.pullSelect = kPORT_PullUp;
-	Test_PIN_Setup.mux = kPORT_MuxAsGpio;
-	Test_PIN_Setup.passiveFilterEnable = kPORT_PassiveFilterDisable;
-	Test_PIN_Setup.driveStrength = kPORT_LowDriveStrength;
-	Test_PIN_Setup.slewRate = kPORT_SlowSlewRate;
-
-	PORT_SetPinConfig(PORTC, 7U, &Test_PIN_Setup);
-
-}
 
 
--- IO/io.h
+++ IO/io.h
@@ -8,12 +8,21 @@
 #ifndef IO_H_
 #define IO_H_
 
-
+#include "fsl_common.h"
+#include "fsl_clock.h"
 #include "fsl_gpio.h"
 #include "fsl_port.h"
 
 typedef enum
+{
+    PORT_A,
+    PORT_B,
+    PORT_C,
+    PORT_D,
+    PORT_E,
+}IO_PORT;
 
-PIN_DIR
+void io_init(void);
+int8_t io_Pin_Cfg(IO_PORT PORT, uint8_t PIN, gpio_pin_direction_t DIR);
 
 #endif /* IO_H_ */
