034f6c94c0387281c1cb90855af6f718b90c8207
eroto <eroto1981@gmail.com>
2017-05-24 22:49:24 -0500

    Add make
    Add make

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -2,43 +2,32 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-ELF_SRCS := 
-O_SRCS := 
-CPP_SRCS := 
-C_UPPER_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
 C_SRCS := 
 S_UPPER_SRCS := 
-OBJ_SRCS := 
-ASM_SRCS := 
-CXX_SRCS := 
-C++_SRCS := 
-CC_SRCS := 
-SECONDARY_SIZE := 
-C++_DEPS := 
+O_SRCS := 
+EXECUTABLES := 
 OBJS := 
 C_DEPS := 
-ASM_DEPS := 
-CC_DEPS := 
-SECONDARY_FLASH := 
-CPP_DEPS := 
-CXX_DEPS := 
-C_UPPER_DEPS := 
-S_UPPER_DEPS := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-utilities \
-startup \
-source \
-drivers \
-board \
-apptasks \
-Timers \
-SerialCom \
-Schr \
-Relay \
-RTC \
-LCD1602A \
-InitApp \
+CMSIS \
 IO \
+InitApp \
+LCD1602A \
+RTC \
+Relay \
+Schr \
+SerialCom \
+Timers \
+apptasks \
+drivers \
+frdmkl25z \
+pm \
+source \
+startup \
+utilities \
 
